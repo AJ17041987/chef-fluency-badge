5a757983678416c2d74e77cf7514b0e3cf576767
AJ17041987 <ajitesh.kumar@mindtree.com>
2017-01-30 01:15:17 +0000

    added minor changes to repository

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,10 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+if node["platform_family"] == "rhel"
+    package = "httpd"
+else
+   package="apache2"
+end
 
-package 'httpd' do
+package package do
+	package_name package
 	action:install
 end
-service 'httpd' do
+
+service package do
+	service_name package
 	action [:enable,:start]
 end
